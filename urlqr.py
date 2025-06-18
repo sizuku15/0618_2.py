ed2dab911c4c3b4457d2640e468e6246dc97eead
sizuku15 <24261001@kyoto-wu.ac.jp>
2025-06-18 11:28:30 +0900

    Add files via upload

---
+++ urlqr.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+import qrcode
+import io
+st.title("URLをQRcodeに変換")
+
+with st.form(key="url-input",clear_on_submit=True):
+    url=st.text_input("URL:")
+    button=st.form_submit_button("変換")
+    
+if url:
+    img=qrcode.make(url)
+    with io.BytesIO() as f:
+        img.save(f,format="PNG")
+        png=f.getbalue()
+    st.write(url)
+    st.image(png)
+    st.download_button("Download",date=png,file_name="urlqr.png")
